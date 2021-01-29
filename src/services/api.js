da26785508c200e93e16c2529fc1fb16342c9c65
Joel Ferreira da Silva <jfsilva@inf.ufsm.br>
2021-01-29 14:09:52 -0300

    WIP start na configuração das chamadas de APIs

---
+++ src/services/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_KEY_YT = 'AIzaSyD-7kWuyTX5kYs4-8LllJciGCs7bii1FJQ';
+
+// events.json?size=1&apikey=
+const API_KEY_TM = 'BJJDuUOy8uTGfnCOsKN5e3ZfxI6EZiNS';
+export const apiYouTube = axios.create({
+  baseURL: '',
+});
+
+export const apiTicketMaster = axios.create({
+  baseURL: 'https://app.ticketmaster.com/discovery/v2/',
+});
