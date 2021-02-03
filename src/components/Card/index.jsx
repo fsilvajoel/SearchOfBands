0723e1139eb75e2329bc6aaa00fe7dbfd498b5de
Joel Ferreira da Silva <jfsilva@inf.ufsm.br>
2021-02-03 02:05:30 -0300

    componente card: refatoração do estilo e adaptação para mobile

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,17 +1,46 @@
-import React from 'react';
-import { Container, Title } from './styles';
+import React, { useState } from 'react';
 
-function CardList({ channelTitle, description, publishedTime, thumbnails, title, id }) {
+import * as moment from 'moment';
+import { Container, Title, Description, Item } from './styles';
+import Button from '@material-ui/core/Button';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import TvIcon from '@material-ui/icons/Tv';
+import ScheduleIcon from '@material-ui/icons/Schedule';
+
+import Modal from '../Modal/index';
+function Card({ channelTitle, description, publishTime, thumbnails, title, id }) {
+  const [open, setOpen] = useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Container>
-      <Title>{channelTitle}</Title>
-      <p>{description}</p>
-      <p>{publishedTime}</p>
-      <img src={thumbnails?.default?.url} alt={title} />
-      <p>{title}</p>
-      <p>{id.videoId}</p>
+      <img src={thumbnails?.high?.url} alt={title} />
+      <Title>{title}</Title>
+      <div className="media-resources">
+        <Item>
+          <TvIcon />
+          <label>{channelTitle}</label>
+        </Item>
+        <Item>
+          <ScheduleIcon />
+          <label>{moment(publishTime).format('DD/MM/YYYY')}</label>
+        </Item>
+        <Button variant="outlined" onClick={handleClickOpen}>
+          <PlayArrowIcon />
+          Play!
+        </Button>
+        <Modal open={open} onClose={handleClose} id={id} title={title} />
+      </div>
+      <p>Descrição do Vídeo</p>
+      <Description>{description}</Description>
     </Container>
   );
 }
 
-export default CardList;
+export default Card;
--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -3,12 +3,47 @@ import styled from 'styled-components';
 export const Container = styled.div`
   color: #282c34;
   background-color: #ffffff;
-  margin: 1px 0 10px 0;
+  height: 700px;
+  margin: 20px;
   border: 1px solid red;
   border-radius: 5px;
-  width: 80vw;
+  width: 20vw;
   transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  img {
+    height: 360px;
+    /* width: 480px; */
+    width: 100%;
+  }
+  .media-resources {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    font-size: 14px;
+  }
+  @media (max-width: 800px) {
+    width: 95vw;
+    flex-direction: column;
+    p {
+      padding-bottom: 5px;
+    }
+    .media-resources {
+      flex-direction: column;
+      align-items: center;
+    }
+  }
 `;
 export const Title = styled.h1`
-  font-size: 18px;
+  font-size: 20px;
+`;
+export const Description = styled.p`
+  font-size: 14px;
+`;
+export const Item = styled.div`
+  display: flex;
+  flex-direction: row;
+  font-size: 20px;
+  svg {
+    padding: 0;
+  }
 `;
