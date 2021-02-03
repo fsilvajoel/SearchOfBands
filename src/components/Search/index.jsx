44323833165d37450f8357422984f703e5702f83
Joel Ferreira da Silva <jfsilva@inf.ufsm.br>
2021-02-03 18:35:48 -0300

    Ajustes na barra de busca

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,37 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
-import searchYoutube from 'youtube-api-v3-search';
+import { useSearch } from '../../context/youTubeContext';
 
 import { Button } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
-
-import { API_KEY_YT, getDataTicketMaster } from '../../services/api';
-
 import { Container } from './styles';
 
 function Search() {
+  const { updateData } = useSearch();
   const { register, handleSubmit } = useForm();
-  const [youTubeData, setYouTubeData] = useState({});
-  const [ticketMasterData, setTicketMasterData] = useState({});
-  const [fetching, setFetching] = useState(0);
-  const findData = (query) => {
-    const optionsForYT = {
-      q: query,
-      part: 'snippet',
-      type: 'video',
-    };
-    setFetching(1);
-    getDataTicketMaster(query).then((res) => {
-      setTicketMasterData(res);
-    });
-    searchYoutube(API_KEY_YT, optionsForYT).then((res) => {
-      setYouTubeData(res.items);
-      setFetching(2);
-    });
-  };
 
   const onSubmit = (data) => {
-    findData(data.search);
+    updateData(data.search);
   };
 
   return (
